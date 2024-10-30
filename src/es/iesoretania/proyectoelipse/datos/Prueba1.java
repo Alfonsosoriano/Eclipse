6f77c8643f2b4136aa6f11e13e6a33519ba7ca6b
Alfonso Soriano <asorrod2604@g.educaand.es>
2024-10-30 09:12:30 +0100

    Creamos prueba 1

--- src/es/iesoretania/proyectoelipse/datos/Prueba1.java
+++ src/es/iesoretania/proyectoelipse/datos/Prueba1.java
@@ -2,4 +2,9 @@ package es.iesoretania.proyectoelipse.datos;
 
 public class Prueba1 {
 
+	public static void main(String[] args) {
+
+		System.out.println("Buenos dias");
+	}
+
 }
--- src/es/iesoretania/proyectoelipse/datos/Prueba2.java
+++
@@ -1,7 +0,0 @@
-package es.iesoretania.proyectoelipse.datos;
-
-public class Prueba2 {
-	public static void main(String[]args) {
-System.out.println("adios, no soy Mateo");
-}
-}
